3d89adef26906a389a10e07e52a86d4677cd0af3
AVINASH PALA <129177426+avinash8096@users.noreply.github.com>
2024-05-09 23:00:29 +0530

    Add files via upload

---
+++ style-switcher.js
@@ -0,0 +1,46 @@
+// toggle style switcher ///
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+// hide style switch on scroll 
+window.addEventListener("scroll", () =>{
+    if(    document.querySelector(".style-switcher").classList.contains("open"))
+        {
+            document.querySelector(".style-switcher").classList.remove("open");
+
+        }
+})
+// ========================theme colou==========r========= ///
+const alternateStyles= document.querySelectorAll(".alternate-style");
+function setActiveStyle(colour)
+{
+    alternateStyles.forEach((style) => {
+        if(colour === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled","true");
+        }
+    })
+}
+// ========================theme light and dark=================== ///
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+        {
+            dayNight.querySelector("i").classList.add("fa-sun");
+        }
+        else
+        {
+            dayNight.querySelector("i").classList.add("fa-moon");
+        }
+})
+
